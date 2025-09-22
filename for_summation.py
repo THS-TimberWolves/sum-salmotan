e96f8b36c65b7bc5c84f02ba7bc54f5ef51b74e2
salmotan <salmotan000@alpinesd.org>
2025-09-22 10:53:09 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,12 @@
 #for Summation code here
+final_num = 0
+num = int(input("number"))
+num2 = num
+while num2 > 0:
+    num = num + (num2 -1)
+    num2 = num2 -1
+print(num)
+num = int(input("number"))
+for x in range(1,num+1,1):
+    final_num += x
+print(final_num)
